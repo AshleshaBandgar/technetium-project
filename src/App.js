814e43b39ee26543584a787c0165982c8cb28677
AshleshaBandgar <ashlesha.bandgar10@gmail.com>
2023-04-20 20:11:22 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,35 +1,51 @@
  import React from 'react';
  import logo from './logo.svg';
  import './App.css';
+ //import MyButton from './components/button';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>
-          Benifits of using react...
-        </h1>
-        <div
-          className="App-link">
-            
-           1.Component-based architecture <br/>
-           2.Virual DOM for efficient updates<br/>
-           3.Rich ecosystem and community<br/>
-          4.cross-platform development<br/>
-          5.strong community support
-        </div><br/>
-        <MyButton/>
-      </header>
-    </div>
-  );
-}
 
-export default App;
-
-function MyButton(){
+ function MyButton(){
   return(
     <button>Get started..</button>
   )
   
 }
+
+function Reactelements(){
+return(
+   <>
+   1.Component-based architecture<br/>
+   2.Virtual DOM for efficient updates<br/>
+   3.Rich ecosystem and community<br/>
+   4.cross-platform development<br/>
+   5.strong community supoort<br/>
+
+   </>
+)
+
+}
+function Reactlogo(){
+  return(
+
+ <img src={logo} className="App-logo" alt="logo" />
+  )
+}
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+      <Reactlogo/>
+      <Reactelements/>
+      <MyButton/>
+
+        
+      </header>
+      </div>
+        
+  );
+}
+
+export default App;
+
