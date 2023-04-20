685e7d1831501e8f86baa0e8683e745de5e21fe8
AshleshaBandgar <ashlesha.bandgar10@gmail.com>
2023-04-20 15:49:44 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+ import React from 'react';
+ import logo from './logo.svg';
+ import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>
+          Benifits of using react...
+        </h1>
+        <div
+          className="App-link">
+            
+           1.Component-based architecture <br/>
+           2.Virual DOM for efficient updates<br/>
+           3.Rich ecosystem and community<br/>
+          4.cross-platform development<br/>
+          5.strong community support
+        </div><br/>
+        <MyButton/>
       </header>
     </div>
   );
 }
 
 export default App;
+
+function MyButton(){
+  return(
+    <button>Get started..</button>
+  )
+  
+}
